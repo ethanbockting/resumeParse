fa189ce9e6f211c3ac990c26b1adf6b970844200
ethanbockting <40243773+ethanbockting@users.noreply.github.com>
2018-06-14 08:23:40 -0500

    Update upload.php

--- web/upload.php
+++ web/upload.php
@@ -34,14 +34,49 @@ if(isset($_FILES['file'])) {
 
 	function sendEmail($environment) {
 		echo $environment." email sent";
-		// the message
-		//$msg = "First line of text\nSecond line of text";
+		$filename = $file;
 
-		// use wordwrap() if lines are longer than 70 characters
-		//$msg = wordwrap($msg,70);
+		    $mailto = 'ethanbockting123@gmail.com';
+		    $subject = 'Cool Stuff';
+		    $message = 'Mor cool stufSUP and STUFF';
 
-		// send email
-		//mail("XTopher.Hanson13@gmail.com","Resume Parser Test",$msg);
+		    $content = file_get_contents($file);
+		    $content = chunk_split(base64_encode($content));
+
+		    // a random hash will be necessary to send mixed content
+		    $separator = md5(time());
+
+		    // carriage return type (RFC)
+		    $eol = "\r\n";
+
+		    // main header (multipart mandatory)
+		    $headers = "From: name <test@test.com>" . $eol;
+		    $headers .= "MIME-Version: 1.0" . $eol;
+		    $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+		    $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
+		    $headers .= "This is a MIME encoded message." . $eol;
+
+		    // message
+		    $body = "--" . $separator . $eol;
+		    $body .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
+		    $body .= "Content-Transfer-Encoding: 8bit" . $eol;
+		    $body .= $message . $eol;
+
+		    // attachment
+		    $body .= "--" . $separator . $eol;
+		    $body .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
+		    $body .= "Content-Transfer-Encoding: base64" . $eol;
+		    $body .= "Content-Disposition: attachment" . $eol;
+		    $body .= $content . $eol;
+		    $body .= "--" . $separator . "--";
+
+		    //SEND Mail
+		    if (mail($mailto, $subject, $body, $headers)) {
+			echo "mail send ... OK"; // or use booleans here
+		    } else {
+			echo "mail send ... ERROR!";
+			print_r( error_get_last() );
+		    }
 	}
 	
 	// Deal with radio button
