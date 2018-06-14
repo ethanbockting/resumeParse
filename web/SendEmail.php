7243a13e38e26e3b815919400134f2f1419e028d
mentin <mentin@iastate.edu>
2018-06-14 09:57:28 -0500

    final email commit

--- web/SendEmail.php
+++ web/SendEmail.php
@@ -7,41 +7,44 @@ require './PHPMailer/src/Exception.php';
 require './PHPMailer/src/PHPMailer.php';
 require './PHPMailer/src/SMTP.php';
 
-// these should all be strings my doods
-function SendEmail($from, $to, $fromName, $body, $subject, $file)
+public class EmailSending()
 {
-	$email = new PHPMailer();
-	$email->From      = $from;
-	$email->FromName  = $fromName;
-	$email->Subject   = $subject;
-	$email->Body      = $body;
-	$email->AddAddress( $to );
-	
-	$email->AddAttachment( $file , 'resume' );
+		// these should all be strings my doods
+	public function SendEmail($from, $to, $fromName, $body, $subject, $file)
+	{
+		$email = new PHPMailer();
+		$email->From      = $from;
+		$email->FromName  = $fromName;
+		$email->Subject   = $subject;
+		$email->Body      = $body;
+		$email->AddAddress( $to );
+		
+		$email->AddAttachment( $file , 'resume' );
 
-	return $email->Send();
+		return $email->Send();
+	}
+
+
+	public function SendTestEmail()
+	{
+		// TODO replace with actual email information
+		$email = new PHPMailer();
+		$email->From      = 'michaelrentin@gmail.com';
+		$email->FromName  = 'Your Name';
+		$email->Subject   = 'Message Subject';
+		$email->Body      = 'texties';
+		$email->AddAddress( 'michaelrentin@gmail.com' );
+
+		// replace this with actual document
+		$file_to_attach = '/Users/michaelentin/Downloads/test.docx';
+
+		// rename the thing here
+		$email->AddAttachment( $file_to_attach , 'picture' );
+
+		return $email->Send();
+	}
 }
 
 
-function SendTestEmail()
-{
-	// TODO replace with actual email information
-	$email = new PHPMailer();
-	$email->From      = 'michaelrentin@gmail.com';
-	$email->FromName  = 'Your Name';
-	$email->Subject   = 'Message Subject';
-	$email->Body      = 'texties';
-	$email->AddAddress( 'michaelrentin@gmail.com' );
-
-	// replace this with actual document
-	$file_to_attach = '/Users/michaelentin/Downloads/test.docx';
-
-	// rename the thing here
-	$email->AddAttachment( $file_to_attach , 'picture' );
-
-	return $email->Send();
-}
-
-SendTestEmail();
 
 ?>
