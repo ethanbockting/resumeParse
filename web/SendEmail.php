36e40888c6cf10ac9e024c66c8cdfad22ec71cac
XTopher13 <40207524+XTopher13@users.noreply.github.com>
2018-06-14 14:05:34 -0500

    Update SendEmail.php

--- web/SendEmail.php
+++ web/SendEmail.php
@@ -5,9 +5,9 @@ echo "SendEmail included";
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require $_SERVER['DOCUMENT_ROOT'].'/web/PHPMailer/src/Exception.php';
-require $_SERVER['DOCUMENT_ROOT'].'/web/PHPMailer/src/PHPMailer.php';
-require $_SERVER['DOCUMENT_ROOT'].'/web/PHPMailer/src/SMTP.php';
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
 
 public class EmailSending()
 {
